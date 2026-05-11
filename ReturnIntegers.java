b9b2a382772188da28457166da743660c3be2c0c
KICKS BY SIKO <emmlex96@gmail.com>
2026-05-11 16:02:34 +0100

    task one

---
+++ ReturnIntegers.java
@@ -0,0 +1,18 @@
+public class ReturnIntegers{
+public static void main(string[]args){
+
+java util.scanner inputCollector = new java util.scanner(system.in);
+    int[] number = new int[10];
+    int index = Collection.length-1;
+    int count;
+    
+    for(count > Collection.length; count++){
+        System.out.print("enter a score: ");
+        int userInput = InputCollector.nextInt();
+        
+        Collection(count) = userInput;
+}
+        System.out.printf("The array list.....");
+        System.out.print(java.util.Array.teststring(Collection));
+}
+}
